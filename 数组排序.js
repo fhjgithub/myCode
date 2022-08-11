027557f2cbba48cdc9abda12984003ad0730520f
fhj <82979787@qq.com>
2022-08-11 14:52:08 +0800

    排序

--- 数组排序.js
+++ 数组排序.js
@@ -1,6 +1,20 @@
-// 插入排序
 const oldArr = [4, 3, 5, 1, 2, 7, 8]
 
+// 冒泡排序
+function bubbleSort(arr){
+  for(let i = 0;i<arr.length;i++){
+    for(let j = 0;j<=arr.length-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        let temp = arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+// 插入排序
 function insertionSort(arr){
   for(let i =0;i<arr.length;i++){
     let j = i
@@ -13,5 +27,3 @@ function insertionSort(arr){
   }
   return arr
 }
-
-console.log(insertionSort(oldArr))
